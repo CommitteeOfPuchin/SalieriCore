ee13d7764b66592eea639bbf125327cf9be045fa
Atikin Vozhich <chizhovnikita2000@gmail.com>
2018-08-19 23:44:22 +0300

    Хотфикс (#1)

--- Kurisu/main.py
+++ Kurisu/main.py
@@ -4,7 +4,7 @@ webhook = '<url>'
 import discord, asyncio, urllib.request, os.path, sqlite3, copy, time, datetime
 
 import kurisu.nyaa, kurisu.tips, kurisu.override, kurisu.alpaca, kurisu.prefs, kurisu.tasks
-import traceback
+import traceback, requests, signal, sys
 
 from discord.ext import commands
 
@@ -16,7 +16,6 @@ client = commands.Bot(command_prefix='!', description='Amadeus Systems', formatt
 ready = False
 taskList = {}
 
-
 def sigint_handler(sig, frame):
 	req = requests.post(webhook, json={'embeds': [{'color': '15158332', 'title': 'Процесс `Amadeus` был закрыт.', 'description': 'Причина: Keyboard Interrupt'}]})
 	sys.exit(0)
@@ -26,6 +25,21 @@ signal.signal(signal.SIGINT, sigint_handler)
 @client.event
 async def on_ready():
 	global ready
+
+	if ready == True:
+		await client.send_message(kurisu.prefs.Channels.dev, "Переподключение...")
+	else:
+		print('[Discord] | Initializing tips')
+		kurisu.tips.init()
+		print('[Discord] | Initializing preferences')
+		kurisu.prefs.discordClient = client
+		kurisu.prefs.init()
+		print('[Discord] | Logged in as: %s | %s' % (client.user.name, client.user.id))
+		await client.change_presence(game=discord.Game(name='Steins;Gate 0', type=3))
+		kurisu.tasks.loop = client.loop
+		await kurisu.tasks.new(kurisu.nyaa.fetch)
+		await kurisu.tasks.new(kurisu.alpaca.alpacaLoop)
+		kurisu.prefs.startup = datetime.datetime.now()
 
 	desc = '{u.mention} готова к работе'.format(u=client.user)
 	req = requests.post(webhook, json={'embeds': [{'color': '3066993', 'title': 'Процесс `Amadeus` запущен.', 'description': desc}]})
